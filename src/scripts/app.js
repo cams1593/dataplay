e63353ebd7ac22ea963689303711567a4856983a
Gasp763 <gaspardberthe@gmail.com>
2026-03-17 17:31:48 +0100

    Ajout et commencement Json

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,5 @@
-console.info('Hello world');
+const url = '../Json/annees.json';
+
+const data = fetch(url).then(data => data.json()).then((response) =>{
+    console.log(response.top_10[0].topwords[8].occurences);
+})
