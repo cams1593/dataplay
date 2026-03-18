7d56176cece8bab43d063012e6a55506a8f1c0b6
Gasp763 <gaspardberthe@gmail.com>
2026-03-18 17:35:21 +0100

    Commencement bouton qui tourne

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,12 +1,11 @@
 const url = '../Json/annees.json';
-
 fetch(url)
     .then(response => response.json())
     .then((data) => {
 //        console.log(data.top_10[0].title);
 //        console.log(data.top_10[0].rang);
 
-        console.log(data.y_1980.top_10[6].topwords[2]);
+//        console.log(data.y_1980.top_10[6].topwords[2]);
 
 
 
@@ -16,3 +15,40 @@ fetch(url)
 //        });
     })
     .catch(error => console.error("Erreur du fetch :", error));
+
+//----------------Le Bouton Qui Tourne-------------------
+
+const knob = document.querySelector(".bigknob__center");
+const knobRect = knob.getBoundingClientRect();
+const knobX = knobRect.left + knobRect.width / 2 ;
+
+let mousePosition = 0;
+
+knob.addEventListener("mousedown", onKnobClick);
+
+
+
+
+
+function onKnobClick () {
+    document.addEventListener("mousemove", onMouseMove);
+}
+function onMouseMove (event) {
+    const click = event.clientX;
+    
+    const root = document.querySelector(":root");
+    const style = getComputedStyle(root);
+    const rotate = style.getPropertyValue("--rotateValue");
+    
+    if (mousePosition > click) {
+        console.log("tu vas à gauche");
+        
+    }
+    if (mousePosition < click) {
+        console.log("tu vas à droite");
+
+        document.documentElement.style.setProperty("--textColor", "1deg");
+        console.log(rotate);
+    }
+    mousePosition = click;
+}
