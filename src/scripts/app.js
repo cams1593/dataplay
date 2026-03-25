e15b0e3df57e94488179c36585bd3bf37ba79124
Gasp763 <gaspardberthe@gmail.com>
2026-03-25 14:57:11 +0100

    Page crédits + suppression du fetch au début qui servait à rien

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -4,22 +4,22 @@ import { color } from 'chart.js/helpers';
 const url = '../Json/annees.json';
 let myGraph = null;
 let graphDonut = null;
-fetch(url)
-    .then(response => response.json())
-    .then((data) => {
-//        console.log(data.top_10[0].title);
-//        console.log(data.top_10[0].rang);
-
-//        console.log(data.y_1980.top_10[6].topwords[2]);
-
-
-
-        // Pour lister tous les titres du top_10
-//        data.top_10.forEach(chanson => {
-//            console.log(`Rang ${chanson.rang} : ${chanson.title}`);
-//        });
-    })
-    .catch(error => console.error("Erreur du fetch, l'année choisie n'est pas disponible:", error));
+//fetch(url)
+//    .then(response => response.json())
+//    .then((data) => {
+////        console.log(data.top_10[0].title);
+////        console.log(data.top_10[0].rang);
+//
+////        console.log(data.y_1980.top_10[6].topwords[2]);
+//
+//
+//
+//        // Pour lister tous les titres du top_10
+////        data.top_10.forEach(chanson => {
+////            console.log(`Rang ${chanson.rang} : ${chanson.title}`);
+////        });
+//    })
+//    .catch(error => console.error("Erreur du fetch, l'année choisie n'est pas disponible:", error));
 
 //----------------Le Bouton Qui Tourne-------------------
 const pageId = document.body.id;
